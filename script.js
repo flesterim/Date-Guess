1ecc32c29d7f39b2612979eadea2c1e4c10e57bd
lesterim <lester11inga@gmail.com>
2026-04-20 18:58:15 -0500

    avance de la logica

--- script.js
+++ script.js
@@ -14,8 +14,12 @@ Teclas.forEach(boton => {
 })
 
 function actualizarInterfaz(valor){
-    let valorCelda = document.getElementById(`celda${nIntento}-${intentoUsuario.length}`)
-    valorCelda.textContent = valor
+    let idBusqueda = `celda${nIntento}-${intentoUsuario.length}`
+    let valorCelda = document.getElementById(idBusqueda)
+
+    if (valorCelda) {
+        valorCelda.textContent = valor
+    }
 }
 
 function generarFecha() {
@@ -31,22 +35,27 @@ function generarFecha() {
     const stringFecha = dia + mes + anio
 
     return stringFecha.split('').map(Number);
-
 }
 
-const fechaRandom = [2,4,1,1,2,0,0,4]
-const intento = [1,7,1,2,2,0,1,9]
+const fechaEstablecida = generarFecha()
+console.log(fechaEstablecida)
 let coloresIntento = []
 
 function compararFecha(correcta, intento) {
     for (let i = 0; i < intento.length; i++) {
         if (intento[i] == correcta[i]) {
-            coloresIntento[i] = "v" // verde
+            coloresIntento[i] = "v" // se debe pintar de verde
+            let celdita = document.getElementById(`celda${nIntento}-${intento[i] + 1}`)
+            celdita.classList.add("verde")
         } else {
-            coloresIntento[i] = "g" // gris
+            coloresIntento[i] = "g" // se debe pintar de gris
+            let celdita = document.getElementById(`celda${nIntento}-${intento[i] + 1}`)
+            celdita.classList.add("gris")
             for (let j = 0; j < correcta.length; j++) {
                 if (intento[i] == correcta[j]) {
-                    coloresIntento[i] = "a" // amarillo
+                    coloresIntento[i] = "a" // se debe pintar de amarillo
+                    let celdita = document.getElementById(`celda${nIntento}-${intento[i] + 1}`)
+                    celdita.classList.add("amarillo")
                     break;
                 }
             }
@@ -54,10 +63,25 @@ function compararFecha(correcta, intento) {
     }
 }
 
-compararFecha(fechaRandom, intento)
+let botonEnviarId = document.getElementById("tecla-enviar")
+botonEnviarId.addEventListener("click", ()=> {
+    if (intentoUsuario.length == 8) {
+        const coloresCalculados = compararFecha(fechaEstablecida, intentoUsuario)
+
+        coloresCalculados.forEach((color, index) => {
+            let celdita = document.getElementById(`celda${nIntento}-${index + 1}`)
+            if (color == "v") {
+                celdita.classList.add("verde")
+            } else if (color == "g"){
+                celdita.classList.add("gris")
+            } else {
+                celdita.classList.add("amarillo")
+            }
+        });
+    }
+})
 
 for (let a = 0; a < coloresIntento.length; a++) {
     console.log(coloresIntento[a])
 }
 
-console.log(generarFecha())
